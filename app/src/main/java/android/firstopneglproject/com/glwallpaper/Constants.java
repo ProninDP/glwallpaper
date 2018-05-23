0a34407a3c6099361cf778e0f57eee4cb1d0893d
Dmitry Pronin <slidernode@yandex.ru>
2018-05-23 11:17:46 +0300

    Start

---
+++ app/src/main/java/android/firstopneglproject/com/glwallpaper/Constants.java
@@ -0,0 +1,5 @@
+package android.firstopneglproject.com.glwallpaper;
+
+public class Constants {
+    public static final int BYTES_PER_FLOAT = 4;
+}
---
+++ app/src/main/java/android/firstopneglproject/com/glwallpaper/util/LoggerConfig.java
@@ -0,0 +1,5 @@
+package android.firstopneglproject.com.glwallpaper.util;
+
+public class LoggerConfig {
+    public static final boolean ON = true;
+}
